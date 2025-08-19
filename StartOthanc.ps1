7e0222f758013dba2160804eccfe01535aa0ef62
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2025-08-19 02:57:26 -0700

    Add files via upload

---
+++ StartOthanc.ps1
@@ -0,0 +1,6 @@
+﻿# Define the path to the Orthanc executable and the config file
+$orthancPath = "C:\Program Files\Orthanc Server\Orthanc.exe"
+$configPath = "orthanc.json"
+
+# Start the Orthanc server with the config file
+Start-Process -FilePath $orthancPath -ArgumentList $configPath
